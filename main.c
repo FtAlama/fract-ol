b623741ab925a3c254b820cf084b878f1bc83a01
alama <ajitsonam999@gmail.com>
2024-06-03 22:49:25 +0200

    image test

--- main.c
+++ main.c
@@ -6,11 +6,22 @@
 /*   By: alama <alama@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:17:57 by alama             #+#    #+#             */
-/*   Updated: 2024/06/03 19:21:43 by alama            ###   ########.fr       */
+/*   Updated: 2024/06/03 22:23:01 by alama            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	set_image(t_data *data)
+{
+	int	p_bits;
+	int	l_bytes;
+	int	end;
+
+	data->buff_img = mlx_get_data_addr(data->img, &p_bits, &l_bytes, &end);
+}
+
+
 
 int	main(void)
 {
@@ -24,6 +35,9 @@ int	main(void)
 	data->h = 980;
 //	mlx_get_screen_size(data->mlx, &data->w, &data->h);
 	data->win = mlx_new_window(data->mlx, data->w, data->h, "fract'ol");
+	data->img = mlx_new_image(data->mlx, data->w, data->h);
+	set_image(data);
+	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
 	data->err_output = 0;
 	mlx_key_hook(data->win, &key_event, data);
 	mlx_hook(data->win, 17, 0, &out_prog, data);
